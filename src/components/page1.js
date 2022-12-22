ffa4cf193b5a457f71108c79e17bd99d25c8f9aa
TongSay <tongsayofficail@gmail.com>
2022-12-22 14:20:54 +0700

    merge pro

--- src/components/page1.js
+++ src/components/page1.js
@@ -6,11 +6,12 @@ function Page1 () {
       }
     return (
         <div>
+            <h3>This is branch Pro</h3>
             <h3>This is Hello Component</h3>
             <h3>This is Hello Component</h3>
             <h3>This is Hello Component</h3>
             <h3>This is Hello Component</h3>
-            
+            <button onClick={page1}>Plese Click me!</button>
         </div>
     )
 }
